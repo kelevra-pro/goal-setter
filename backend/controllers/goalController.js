82fd71049155f72ee8d451604985a45a5beb89fa
kelevra-pro <damtat8@gmail.com>
2022-02-26 23:09:58 +0300

    add user routes and controller functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,18 +2,18 @@ const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
 
-// @desc  Get goals
-// @route GET /api/goals
-// @access Private
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find();
 
   res.status(200).json(goals);
 });
 
-// @desc  Set goals
-// @route POST /api/goals
-// @access Private
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
@@ -27,9 +27,9 @@ const setGoal = asyncHandler(async (req, res) => {
   res.status(200).json(goal);
 });
 
-// @desc  Update goal
-// @route PUT /api/goals/:id
-// @access Private
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
@@ -42,9 +42,9 @@ const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json(updatedGoal);
 });
 
-// @desc  Delete goal
-// @route DELETE /api/goals/:id
-// @access Private
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
