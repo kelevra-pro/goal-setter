aa4ed94aa9dcea9f37b11011ee957d43e05852fe
kelevra-pro <damtat8@gmail.com>
2022-03-01 00:59:24 +0300

    add setting a user goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,6 +5,6 @@ const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/go
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getGoals).post(protect, setGoal);
-router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal);
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
