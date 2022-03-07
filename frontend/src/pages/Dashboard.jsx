c670d9f872286946af247953b31049cf868ced48
kelevra-pro <damtat8@gmail.com>
2022-03-07 23:52:52 +0300

    restrict the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const user = useSelector((state => state.auth.user));
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return (
     <div>Dashboard</div>
   );
